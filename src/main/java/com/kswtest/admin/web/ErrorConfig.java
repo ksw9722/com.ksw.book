5f04c496e627f33fc23a6c55439669112ae82ee7
ksw9722 <ksw9722@gmail.com>
2020-10-20 01:14:57 +0900

    Error Page Commit

---
+++ src/main/java/com/kswtest/admin/web/ErrorConfig.java
@@ -0,0 +1,47 @@
+package com.kswtest.admin.web;
+
+import org.springframework.boot.autoconfigure.webservices.WebServicesProperties;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+
+@Controller
+public class ErrorConfig implements ErrorController {
+    private final String PATH = "/error/";
+    private String VIEW_PATH = "/errors";
+
+    @RequestMapping(value=PATH)
+    public String error(HttpServletRequest request){
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if(status!=null){
+            int statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()){
+                return PATH+404;
+            }
+
+            if(statusCode==500){
+                return PATH+500;
+            }
+            return "error";
+
+        }else{
+            return "error";
+        }
+    }
+
+    @Override
+    public String getErrorPath(){
+        return "/error";
+    }
+
+}
---
+++ src/main/java/com/kswtest/admin/web/HelloController.java
@@ -0,0 +1,25 @@
+package com.kswtest.admin.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/error/404")
+    public String error404(){
+        return "ERROR404!!";
+    }
+
+
+
+    @GetMapping("/error/500")
+    public String error500(){
+        return "ERROR500!!";
+    }
+}
