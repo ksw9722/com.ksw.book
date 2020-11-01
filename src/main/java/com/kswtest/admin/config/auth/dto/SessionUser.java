977ee0dc6e3d4b266ec0a96f87382d3d3d128dcc
ksw9722 <ksw9722@gmail.com>
2020-11-01 12:31:35 +0900

    게시판 완성 - postgres 추가5

--- src/main/java/com/kswtest/admin/config/auth/dto/SessionUser.java
+++ src/main/java/com/kswtest/admin/config/auth/dto/SessionUser.java
@@ -1,6 +1,6 @@
 package com.kswtest.admin.config.auth.dto;
 
-import com.kswtest.admin.domain.user.User;
+import com.kswtest.admin.domain.user.user;
 import lombok.Getter;
 
 import java.io.Serializable;
@@ -10,7 +10,7 @@ public class SessionUser implements Serializable {
     private String email;
     private String picture;
 
-    public SessionUser(User user){
+    public SessionUser(user user){
         this.name = user.getName();
         this.email = user.getEmail();
         this.picture = user.getPicture();
